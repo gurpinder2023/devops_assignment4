dc673dafd121cd68049b833310921fcf6519e23f
gurpinder2023 <gurpinderk714@gmail.com>
2024-11-04 21:12:44 -0800

    changed the port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 8080;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
